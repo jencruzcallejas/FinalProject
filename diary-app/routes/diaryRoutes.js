1628902e0502d6c8ae62d692da148a856e12f139
Jennifer Cruz <jennifer.cruz3@wsu.edu>
2025-04-13 01:19:16 -0700

    Create middleware to protect routes
    - create authMiddleware.js
    - update diaryRoutes.js

--- diary-app/routes/diaryRoutes.js
+++ diary-app/routes/diaryRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { ensureAuthenticated } from "../middleware/authMiddleware.js";
 import {
     createEntry,
     getAllEntries,
@@ -12,34 +13,34 @@ const router = express.Router();
 * @desc Fetch all diary entries
 * @access Public (Authentication will be added in Part 2)
 */
-router.get("/", getAllEntries);
+router.get("/", ensureAuthenticated, getAllEntries);
 
 /**
 * @route GET /api/diary/:id
 * @desc Fetch a specific diary entry by ID
 * @access Public (Authentication will be added in Part 2)
 */
-router.get("/:id", getEntryById);
+router.get("/:id", ensureAuthenticated, getEntryById);
 
 /**
  * @route POST /api/diary
  * @desc Create a new diary entry
  * @access Public
  */
-router.post("/", createEntry);
+router.post("/", ensureAuthenticated, createEntry);
 
 /**
  * @route PUT /api/diary/:id
  * @desc Update an existing diary entry
  * @access Public
  */
-router.put("/:id", updateEntry);
+router.put("/:id", ensureAuthenticated, updateEntry);
 
 /**
  * @route DELETE /api/diary/:id
  * @desc Delete an existing diary entry
  * @access Public
  */
-router.delete("/:id", deleteEntry);
+router.delete("/:id", ensureAuthenticated, deleteEntry);
 
 export default router;
